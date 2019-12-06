cfd6d89b500b20b8f72864348c137089a1da67e2
Naumaan Hassan <naumaan.hassan@gmail.com>
2019-12-05 21:35:26 -0600

    updating times

--- webscraper.py
+++ webscraper.py
@@ -295,7 +295,7 @@ allFajr = {
 	'Mckinneya': Mckinneyf,
 	'Mckinneyi': mckinneyIqamahTiming[5].text,
 	'Mansfi': mansF,
-	'Darelimaani': darelimanIqamahTimings[1].text.
+	'Darelimaani': darelimanIqamahTimings[1].text,
 
 }
 allDhur = {
@@ -323,7 +323,7 @@ allDhur = {
 	'Mckinneya': Mckinneyd,
 	'Mckinneyi': mckinneyIqamahTiming[8].text,
 	'Mansfi': mansfIqamahTiming[1].text,
-	'Darelimaani': darelimanIqamahTimings[3].text.
+	'Darelimaani': darelimanIqamahTimings[3].text,
 }
 allAsr = {
 	'ID': 3,
@@ -350,7 +350,7 @@ allAsr = {
 	'Mckinneya': Mckinneya,
 	'Mckinneyi': mckinneyIqamahTiming[11].text,
 	'Mansfi': mansfIqamahTiming[3].text,
-	'Darelimaani': darelimanIqamahTimings[7].text.
+	'Darelimaani': darelimanIqamahTimings[7].text,
 
 
 }
@@ -379,7 +379,7 @@ allMaghrib = {
 	'Mckinneya': Mckinneym,
 	'Mckinneyi': mckinneyIqamahTiming[14].text,
 	'Mansfi': mansfIqamahTiming[5].text,
-	'Darelimaani': darelimanIqamahTimings[9].text.
+	'Darelimaani': darelimanIqamahTimings[9].text,
 
 }
 allIsha = {
@@ -407,7 +407,7 @@ allIsha = {
 	'Mckinneya': Mckinneyi,
 	'Mckinneyi': mckinneyIqamahTiming[17].text,
 	'Mansfi': mansfIqamahTiming[7].text,
-	'Darelimaani': darelimanIqamahTimings[11].text.
+	'Darelimaani': darelimanIqamahTimings[11].text,
 
 }
 
