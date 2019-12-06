80c52e69128919f4ac40c872edc583ebdd5cdb86
Naumaan Hassan <naumaan.hassan@gmail.com>
2019-12-05 22:04:58 -0600

    updating times

--- webscraper.py
+++ webscraper.py
@@ -79,7 +79,7 @@ darelimansoup = BeautifulSoup(darelimanR, "html.parser")
 maisoup = BeautifulSoup(maiHTML, "html.parser")
 
 #MASJID AL ISLAM
-maiIQamahTimings = maisoup.findAll('h3', attrs={"class": "prayerTime"})
+maiIQamahTimings = maisoup.findAll('html')
 print maiIQamahTimings
 
 #DAR EL IMAAN ARLINGTON 
