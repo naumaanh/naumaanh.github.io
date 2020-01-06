0c224daeaf2a3dac42e8a2f5497f8f8cc2a16e3a
Naumaan Hassan <naumaan.hassan@gmail.com>
2020-01-05 19:05:56 -0600

    updating times

--- webscraper.py
+++ webscraper.py
@@ -209,6 +209,8 @@ iciJummahTimings = (icisoup.findAll('td', attrs={"colspan": "2"})[-1])
 iciJJ = ((str(iciJummahTimings)))
 iciJ4 = iciJJ.replace(u"\u2019", "'")
 iciJ3 = iciJ4.replace("1ST JUMUA ", "")
+iciJ3i = iciJ3.replace("     ", "")
+iciJ1 = iciJ3.split(" | ")
 print iciJ3
 #iciAdhanTimingsAsDictionary = {
 #	"FajrAdhan": iciAdhanTimings[0].text,
