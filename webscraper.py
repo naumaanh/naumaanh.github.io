78bccdb387584db6cbedadbbad3cd1e3276b7d0c
Naumaan Hassan <naumaan.hassan@gmail.com>
2021-01-25 23:29:06 -0600

    updating files

--- webscraper.py
+++ webscraper.py
@@ -98,7 +98,7 @@ icopsoup = BeautifulSoup(icopHTML, "html.parser")
 iacsoup = BeautifulSoup(iacHTML, "html.parser")
 
 #Masjid AR RAHMAN ISLAMIC ASSOCIATION OF CARROLLTON IAC
-iacTimings = iacsoup.findAll('td')
+iacTimings = iacsoup.findAll('span style')
 print(iacTimings)
 
 #ISLAMIC ASSOCIATION OF FORT WORTH DAR UN NOOR
